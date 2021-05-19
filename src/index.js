64374f1cf0e142702a70b75b8726399de4bbf0e9
Yago Fontoura <yagofontorua222@gmail.com>
2021-05-19 00:14:11 -0300

    add middleware & Project_Controller

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-require('./controllers/authController')(app);
+require('./controllers/authController.js')(app);
+require('./controllers/projectController')(app);
 
 app.listen(3000);
 
